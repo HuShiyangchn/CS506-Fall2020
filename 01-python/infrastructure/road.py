abc2afe8be2ce9532ba82e685a2fc3ee79fa8d6d
HuShiyangchn <55211567+HuShiyangchn@users.noreply.github.com>
2020-09-17 11:43:43 -0400

    build museum

--- 01-python/infrastructure/road.py
+++ 01-python/infrastructure/road.py
@@ -1,3 +1,27 @@
 def draw_road():
-    print("road not found")
+    type = 2
+    if type == 0:
+        print("| |")
+        print("| |")
+        print("| |")
+        print("| |")
+        print("| |")
+    elif type == 1:
+        print("    / /")
+        print("   / /")
+        print("  / /")
+        print(" / /")
+        print("/ /")
+    elif type == 2:
+        print("  ****")
+        print(" ****")
+        print("****")
+        print(" ****")
+        print("  ****")
+        print(" *****")
+        print("*****")
+        print(" *****")
+        print("  *****")
+
+    #print("road not found")
     return
