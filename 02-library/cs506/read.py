7bffb29925095c2165f866045d023da46e06447b
HuShiyangchn <55211567+HuShiyangchn@users.noreply.github.com>
2020-09-20 21:15:56 -0400

    implement library

--- 02-library/cs506/read.py
+++ 02-library/cs506/read.py
@@ -3,4 +3,16 @@ def read_csv(csv_file_path):
         Given a path to a csv file, return a matrix (list of lists)
         in row major.
     """
-    raise NotImplementedError()
+
+    csvmatrix = []
+    with open(csv_file_path, "r") as file_object:
+        lines = file_object.readlines()
+        for line in lines:
+            line = line.split(",")
+            for i in range(len(line)):
+                line[i] = eval(line[i])
+            csvmatrix.append(line)
+    print (csvmatrix[1])
+    return csvmatrix
+
+#read_csv("E://github//CS506-Fall2020//02-library//tests//test_files//dataset_1.csv")
--- 02-library/cs506/sim.py
+++ 02-library/cs506/sim.py
@@ -1,13 +1,38 @@
 def euclidean_dist(x, y):
-    raise NotImplementedError()
+    sum = 0
+    for i in range(len(x)):
+        sum = sum + abs(x[i] - y[i])**2
+    return sum**(1/2)
+    #raise NotImplementedError()
 
 def manhattan_dist(x, y):
-    raise NotImplementedError()
+    sum = 0
+    for i in range(len(x)):
+        sum = sum + abs(x[i] - y[i])
+    return sum
+    #raise NotImplementedError()
 
 def jaccard_dist(x, y):
-    raise NotImplementedError()
+    if (x==[] or y == []):
+        return 0
+    jaccard_coe = len(set(x)&set(y))/len(set(x)|set(y))
+    return 1 - jaccard_coe
+    #raise NotImplementedError()
 
 def cosine_sim(x, y):
-    raise NotImplementedError()
+    if (x==[] and y == []):
+        raise ValueError("lengths must not be zero")
+    if(len(x) != len(y)):
+        raise ValueError("lengths must be equal")
+    x_sum = 0
+    y_sum =0
+    xy_sum = 0
+    for i in range(len(x)):
+        x_sum = x_sum + (x[i])**2
+        y_sum = y_sum + (y[i])**2
+        xy_sum = xy_sum + (x[i])*(y[i])
+
+    return xy_sum/(x_sum*y_sum)**(1/2)
+    #raise NotImplementedError()
 
 # Feel free to add more
